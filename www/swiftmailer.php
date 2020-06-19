aa2b9a218b0da946d781a8f2116457bc3fa1a3c4
Tatyaniya <tatyaniya@bigmir.net>
2020-06-19 13:02:12 +0300

    added variables

--- www/swiftmailer.php
+++ www/swiftmailer.php
@@ -3,32 +3,25 @@
 require_once '../vendor/autoload.php';
 require '../config.php';
 
-$transport = (new Swift_SmtpTransport(SMTP, PORT, CERTIFICATE))
-    ->setUsername(SET_USER_NAME)
-    ->setPassword(SET_PASSWORD);
+function send($from, $to) {
+    $transport = (new Swift_SmtpTransport(SMTP, PORT, CERTIFICATE))
+        ->setUsername(SET_USER_NAME)
+        ->setPassword(SET_PASSWORD);
+    $mailer = new Swift_Mailer($transport);
+    // Create the message
+    $message = (new Swift_Message())
+        // Give the message a subject
+        ->setSubject('Swiftmailer is here')
+        // Set the From address with an associative array
+        ->setFrom($from)
+        ->setTo($to)
+        ->setBody('Here is the message itself')
+        // And optionally an alternative body
+        ->addPart('<q>Here is the message itself</q>', 'text/html')
 
+        ->attach(Swift_Attachment::fromPath('index.php'));
+    // Send the message
+    $result = $mailer->send($message);
+    var_dump($result);
+}
 
-$mailer = new Swift_Mailer($transport);
-
-// Create the message
-$message = (new Swift_Message())
-
-    // Give the message a subject
-    ->setSubject('Swiftmailer is here')
-
-    // Set the From address with an associative array
-    ->setFrom([SET_FROM => NAME])
-    ->setTo([SET_TO])
-    ->setBody('Here is the message itself')
-
-    // And optionally an alternative body
-    ->addPart('<q>Here is the message itself</q>', 'text/html')
-
-    // Optionally add any attachments
-    ->attach(Swift_Attachment::fromPath('index.php'));
-
-
-// Send the message
-$result = $mailer->send($message);
-
-var_dump($result);
